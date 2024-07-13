a47d8ce716b0aadd392cc0e003c8a0493a0ecd8a
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2024-07-13 14:42:36 +0600

    first commit

---
+++ src/components/aiSolutionPageComponents/Branding.jsx
@@ -0,0 +1,23 @@
+
+
+const Branding = () => {
+  return (
+    <div className=' box-border p-8 justify-center'>
+      <div className='flex justify-center'>
+      <h1 className=' font-syne font-bold text-xl text-center text-black'>The Best Brands Choose Acumens<br/></h1>
+      </div>
+      <div className="flex flex-col flex-wrap md:flex-row justify-center items-center md:my-10 3xl:gap-28 2xl:gap-30 xl:gap-24 gap-16 mx-auto mt-10">
+      <img src="./jolie-1.webp" className="" loading= "lazy"/>
+      <img src="./caridad.webp" className="" loading= "lazy"/>
+      <img src="./F7.webp" className="" loading= "lazy"/>
+      <img src="./Scuola_Logo_OnlyTop-1.webp" className="" loading= "lazy"/>
+      <img src="./petmania.webp" className="" loading= "lazy"/>
+      <img src="./tecnologia.webp" className="" loading= "lazy"/>
+      </div>
+
+      </div>
+    
+  )
+}
+
+export default Branding
